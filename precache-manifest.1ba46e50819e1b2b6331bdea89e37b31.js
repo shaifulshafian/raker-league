7f4f32a186f3438e8e5b0d1fb2490fc748aa028e
Shaiful Shafian <shaifulshafian@gmail.com>
2020-01-04 02:25:38 +0800

    Updates

---
+++ precache-manifest.1ba46e50819e1b2b6331bdea89e37b31.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7026028e5691b03ab6ebe524511654a6",
+    "url": "/raker-league/index.html"
+  },
+  {
+    "revision": "90d6bcb44a89c8aea812",
+    "url": "/raker-league/static/css/main.aa954f5b.chunk.css"
+  },
+  {
+    "revision": "03c6b3f570017fc27183",
+    "url": "/raker-league/static/js/2.a8ac48ac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/raker-league/static/js/2.a8ac48ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90d6bcb44a89c8aea812",
+    "url": "/raker-league/static/js/main.b916d0ee.chunk.js"
+  },
+  {
+    "revision": "e0baa7cd3f3b5d65f6ff",
+    "url": "/raker-league/static/js/runtime-main.3997d4ac.js"
+  },
+  {
+    "revision": "3e055fb491632cd7be494535d08a13e1",
+    "url": "/raker-league/static/media/Bundesliga.3e055fb4.png"
+  },
+  {
+    "revision": "0723988637eb0bcceb4564d8647b8599",
+    "url": "/raker-league/static/media/Eredivisie.07239886.png"
+  },
+  {
+    "revision": "2be242902f8f7daeaf6ff1f729b60ad1",
+    "url": "/raker-league/static/media/LaLiga.2be24290.png"
+  },
+  {
+    "revision": "4b2af31f18df85fc286831937615225b",
+    "url": "/raker-league/static/media/Ligue1.4b2af31f.png"
+  },
+  {
+    "revision": "2ea36f1c21b081213f172410bba0355b",
+    "url": "/raker-league/static/media/PremierLeague.2ea36f1c.png"
+  },
+  {
+    "revision": "3ee74dc22ac21026487a24b49cf2ccfa",
+    "url": "/raker-league/static/media/SerieA.3ee74dc2.png"
+  }
+]);
